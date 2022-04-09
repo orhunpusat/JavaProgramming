eb12bc192cf1b50f23f46edc929ad3997fd615ba
orhunpusat <orhunpusat3000@gmail.com>
2022-04-09 23:33:20 +0300

    add

---
+++ liveReviewSessions/week13/PersonTest.java
@@ -0,0 +1,16 @@
+package week13;
+
+public class PersonTest {
+
+    public static void main(String[] args) {
+
+        Person personOne = new Person("Mike","Smith",30);
+
+        System.out.println(personOne);
+
+        // can I change this person's name?
+        //   personOne.firstName= "Oscar";     no access to the data to change it
+
+
+    }
+}
