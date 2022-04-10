6efb8261ff138bcb4bd1a8683bb696974176e8e5
orhunpusat <orhunpusat3000@gmail.com>
2022-04-10 03:05:42 +0300

    day 38:Inheritance Continue

---
+++ src/day38_Inheritance/carTask/Car.java
@@ -0,0 +1,66 @@
+package day38_Inheritance.carTask;
+
+public class Car {
+    public String brand, model, color;
+    public int year, miles;
+    public double price;
+
+    public Car(String brand, String model, String color, int year, int miles, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.miles = miles;
+        this.price = price;
+    }
+
+    public void start(){
+        System.out.println(brand+model+" is starting");
+    }
+
+    public void drive(){
+        System.out.println(brand+model+" is driving");
+    }
+
+    /*
+    public void fly(){
+    }
+    fly() is not common for all the cars, and parent class should only contain the common feature of all the subclasses
+    */
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", miles=" + miles +
+                ", price=$" + price +
+                '}';
+    }
+}
+/*
+carTask:
+		Create a class called Car
+			instance variables:
+				brand, model, year, price, color, miles
+
+				add a constructor to set all the fields
+
+			instance methods:
+				start(), drive(),toString()
+
+		Create the following sub classes of Car:
+			1. Toyota:
+					extra methods:
+							reliabile()
+
+			2. BMW:
+					extra methods:
+						breaksDown()
+						racing()
+
+			3. Tesla:
+					extra methods:
+						autoPilot();
+ */
