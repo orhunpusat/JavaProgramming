4d9563e8c73cbf74f737d70f8196f791de83a854
orhunpusat <orhunpusat3000@gmail.com>
2022-04-11 22:09:50 +0300

    day 38:Inheritance Continue

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,12 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Camry","Gray",2020,123456,20000);
+
+        Tesla tesla = new Tesla("Model S","Red",2022,0,94990);
+
+        Bmw bmw = new Bmw("X5","white",2020,2340,30000);
+    }
+}
