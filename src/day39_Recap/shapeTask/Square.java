abc572ce232b1f1a5b160e52d27fda5e31fcd66c
orhunpusat <orhunpusat3000@gmail.com>
2022-04-12 19:01:01 +0300

    day 39:Recap --> square

---
+++ src/day39_Recap/shapeTask/Square.java
@@ -0,0 +1,19 @@
+package day39_Recap.shapeTask;
+
+public class Square {
+
+
+
+
+
+}
+/*
+Square extends Shape:
+	variables:
+		side;
+	Encapsulate the field
+	Add a constructor to set the filed
+	area(): side * side
+	perimeter(): side * 4
+	toString(): side, area, perimeter
+ */
