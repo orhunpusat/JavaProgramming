8b5d94ca5fcc88e40f5558c041e3de4cc0aa01aa
orhunpusat <orhunpusat3000@gmail.com>
2022-04-26 15:50:42 +0300

    day43:tester, developer,teacher,driver class

--- src/day43_Abstraction/employee/Employee.java
+++ src/day43_Abstraction/employee/Employee.java
@@ -8,9 +8,12 @@ public abstract class Employee extends Person {
 
     public Employee(String name, int age, char gender, int id, String jobTitle, double salary) {
         super(name, age, gender);
+        if(id<=0){
+            throw new RuntimeException("Invalid ID"+id);
+        }
         this.id = id;
-        this.jobTitle = jobTitle;
-        this.salary = salary;
+        setJobTitle(jobTitle);
+        setSalary(salary);
     }
 
     public int getId() {
